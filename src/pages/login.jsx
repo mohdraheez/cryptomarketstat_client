5df671576f65777895d7169bcee0dc392b580eba
Mohammed Raheez <42299547+mohdraheez07@users.noreply.github.com>
2023-05-18 12:53:20 +0530

    " removed authentication"

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -10,16 +10,7 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDjPddmvt4bGFW9cLBfK7H9rnQmUbEEHfU",
-  authDomain: "cryptomarketstat.firebaseapp.com",
-  databaseURL: "https://cryptomarketstat-default-rtdb.firebaseio.com",
-  projectId: "cryptomarketstat",
-  storageBucket: "cryptomarketstat.appspot.com",
-  messagingSenderId: "996063393940",
-  appId: "1:996063393940:web:d4f3883185331df3dcdc81",
-  measurementId: "G-1E72SDD0X3"
-};
+
 firebase.initializeApp(firebaseConfig);
 
 function Login() {
